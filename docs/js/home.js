2f7aea831f072b6a5035b518ca56eb87d66bead1
lovefruitmr64-tech <lovefruitmr64@gmail.com>
2026-08-24 18:00:07 +0800

    更新博文

--- docs/js/home.js
+++ docs/js/home.js
@@ -7,28 +7,34 @@ const banner=document.querySelector(".home-banner");
 if(!banner)return;
 
 
+
 const slides=banner.querySelectorAll(".banner-item");
 
 const dots=banner.querySelectorAll(".dot");
 
 
+
 let index=0;
+
+let startX=0;
+
+let autoPlay;
 
 
 
 function showSlide(i){
 
 
-slides.forEach(item=>{
+slides.forEach(slide=>{
 
-item.classList.remove("active");
+slide.classList.remove("active");
 
 });
 
 
-dots.forEach(item=>{
+dots.forEach(dot=>{
 
-item.classList.remove("active");
+dot.classList.remove("active");
 
 });
 
@@ -46,6 +52,10 @@ index=i;
 
 
 
+
+/* 点击圆点 */
+
+
 dots.forEach(dot=>{
 
 
@@ -55,14 +65,25 @@ dot.addEventListener("click",()=>{
 showSlide(Number(dot.dataset.index));
 
 
-});
+resetAuto();
 
 
 });
 
 
+});
 
-setInterval(()=>{
+
+
+
+
+/* 自动播放 */
+
+
+function startAuto(){
+
+
+autoPlay=setInterval(()=>{
 
 
 index++;
@@ -79,6 +100,182 @@ showSlide(index);
 
 
 },5000);
+
+
+}
+
+
+
+function resetAuto(){
+
+
+clearInterval(autoPlay);
+
+startAuto();
+
+
+}
+
+
+
+
+
+/* ===========================
+   鼠标拖动
+=========================== */
+
+
+banner.addEventListener("mousedown",e=>{
+
+
+startX=e.clientX;
+
+
+});
+
+
+
+
+banner.addEventListener("mouseup",e=>{
+
+
+let distance=e.clientX-startX;
+
+
+
+if(distance < -80){
+
+
+index++;
+
+
+if(index>=slides.length){
+
+index=0;
+
+}
+
+
+showSlide(index);
+
+
+resetAuto();
+
+
+}
+
+
+
+if(distance > 80){
+
+
+index--;
+
+
+if(index<0){
+
+index=slides.length-1;
+
+}
+
+
+showSlide(index);
+
+
+resetAuto();
+
+
+}
+
+
+});
+
+
+
+
+
+/* ===========================
+   手机滑动
+=========================== */
+
+
+banner.addEventListener("touchstart",e=>{
+
+
+startX=e.touches[0].clientX;
+
+
+},{passive:true});
+
+
+
+
+
+banner.addEventListener("touchend",e=>{
+
+
+let endX=e.changedTouches[0].clientX;
+
+
+let distance=endX-startX;
+
+
+
+
+if(distance < -80){
+
+
+index++;
+
+
+if(index>=slides.length){
+
+index=0;
+
+}
+
+
+showSlide(index);
+
+
+resetAuto();
+
+
+}
+
+
+
+
+if(distance >80){
+
+
+index--;
+
+
+if(index<0){
+
+index=slides.length-1;
+
+}
+
+
+showSlide(index);
+
+
+resetAuto();
+
+
+}
+
+
+
+},{passive:true});
+
+
+
+
+
+startAuto();
 
 
 
